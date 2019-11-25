ca7de9e40e483ad60572407f8444a773dec685df
sune <sune1309@edu.easj.dk>
2019-11-25 11:16:50 +0100

    Initial commit

---
+++ src/Rectangel.java
@@ -0,0 +1,23 @@
+public class Rectangel {
+    double with = 1;
+    double hieight = 1;
+    double area;
+    double perimiter;
+
+    public double getHieight() {
+        return hieight;
+    }
+
+    public double getWith() {
+        return with;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimiter () {
+        return perimiter;
+    }
+
+}
